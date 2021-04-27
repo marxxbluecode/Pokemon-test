2f209a32834bf50278b446f5c107ce82336c5ada
marxxcodes <marxx171@gmail.com>
2021-04-27 18:20:33 -0300

    pure py

---
+++ pokemon_test.py
@@ -0,0 +1,188 @@
+# Pokemon buzzfeed test
+print("Que tipo de pokemon é você?")
+
+mes = int(input("Qual o mês do seu aniversário?"))
+dia = int(input("Qual o dia do seu aniversário?"))
+
+
+if mes == 1 or mes == 8:
+    if dia == 1 or dia == 9 or dia == 17:
+        print("Você é Ground Eletric") 
+    elif dia == 2 or dia == 10 or dia == 21:
+        print("Você é Ground Poison") 
+    elif dia == 3 or dia == 24:
+        print("Você é Ground Normal") 
+    elif dia == 4 or dia == 12 or dia == 23:
+        print("Você é Ground Dark") 
+    elif dia == 5 or dia == 27:
+        print("Você é Ground Dragon") 
+    elif dia == 6 or dia == 16 or dia == 29:
+        print("Você é Ground Ice") 
+    elif dia == 7 or dia == 18:
+        print("Você é Ground Bug") 
+    elif dia == 8 or dia == 19 or dia == 30:
+        print("Você é Ground Fairy") 
+    elif dia == 11 or dia == 22:
+        print("Você é Ground Ghost") 
+    elif dia == 13 or dia == 26:
+        print("Você é Ground Fire")
+    elif dia == 14 or dia == 25:
+        print("Você é Ground Grass") 
+    elif dia == 15 or dia == 28:
+        print("Você é Ground Water") 
+    elif dia == 20 or dia == 31:
+        print("Você é Ground Rock") 
+    else:
+        print("Dia inválido")
+
+if mes == 2 or mes == 11:
+    if dia == 1 or dia == 9 or dia == 17:
+        print("Você é Fighting Eletric") 
+    elif dia == 2 or dia == 10 or dia == 21:
+        print("Você é Fighting Poison") 
+    elif dia == 3 or dia == 24:
+        print("Você é Fighting Normal") 
+    elif dia == 4 or dia == 12 or dia == 23:
+        print("Você é Fighting Dark") 
+    elif dia == 5 or dia == 27:
+        print("Você é Fighting Dragon") 
+    elif dia == 6 or dia == 16 or dia == 29:
+        print("Você é Fighting Ice") 
+    elif dia == 7 or dia == 18:
+        print("Você é Fighting Bug") 
+    elif dia == 8 or dia == 19 or dia == 30:
+        print("Você é Fighting Fairy") 
+    elif dia == 11 or dia == 22:
+        print("Você é Fighting Ghost") 
+    elif dia == 13 or dia == 26:
+        print("Você é Fighting Fire")
+    elif dia == 14 or dia == 25:
+        print("Você é Fighting Grass") 
+    elif dia == 15 or dia == 28:
+        print("Você é Fighting Water") 
+    elif dia == 20 or dia == 31:
+        print("Você é Fighting Rock") 
+    else:
+        print("Dia inválido")
+
+if mes == 3 or mes == 7:    
+    if dia == 1 or dia == 9 or dia == 17:
+        print("Você é Normal Eletric") 
+    elif dia == 2 or dia == 10 or dia == 21:
+        print("Você é Normal Poison") 
+    elif dia == 3 or dia == 24:
+        print("Você é Normal Normal") 
+    elif dia == 4 or dia == 12 or dia == 23:
+        print("Você é Normal Dark") 
+    elif dia == 5 or dia == 27:
+        print("Você é Normal Dragon") 
+    elif dia == 6 or dia == 16 or dia == 29:
+        print("Você é Normal Ice") 
+    elif dia == 7 or dia == 18:
+        print("Você é Normal Bug") 
+    elif dia == 8 or dia == 19 or dia == 30:
+        print("Você é Normal Fairy") 
+    elif dia == 11 or dia == 22:
+        print("Você é Normal Ghost") 
+    elif dia == 13 or dia == 26:
+        print("Você é Normal Fire")
+    elif dia == 14 or dia == 25:
+        print("Você é Normal Grass") 
+    elif dia == 15 or dia == 28:
+        print("Você é Normal Water") 
+    elif dia == 20 or dia == 31:
+        print("Você é Normal Rock") 
+    else:
+        print("Dia inválido")
+
+if mes == 4 or mes == 12:
+    if dia == 1 or dia == 9 or dia == 17:
+        print("Você é Steel Eletric") 
+    elif dia == 2 or dia == 10 or dia == 21:
+        print("Você é Steel Poison") 
+    elif dia == 3 or dia == 24:
+        print("Você é Steel Normal") 
+    elif dia == 4 or dia == 12 or dia == 23:
+        print("Você é Steel Dark") 
+    elif dia == 5 or dia == 27:
+        print("Você é Steel Dragon") 
+    elif dia == 6 or dia == 16 or dia == 29:
+        print("Você é Steel Ice") 
+    elif dia == 7 or dia == 18:
+        print("Você é Steel Bug") 
+    elif dia == 8 or dia == 19 or dia == 30:
+        print("Você é Steel Fairy") 
+    elif dia == 11 or dia == 22:
+        print("Você é Steel Ghost") 
+    elif dia == 13 or dia == 26:
+        print("Você é Steel Fire")
+    elif dia == 14 or dia == 25:
+        print("Você é Steel Grass") 
+    elif dia == 15 or dia == 28:
+        print("Você é Steel Water") 
+    elif dia == 20 or dia == 31:
+        print("Você é Steel Rock") 
+    else:
+        print("Dia inválido")
+
+elif mes == 5 or mes == 9:
+    if dia == 1 or dia == 9 or dia == 17:
+        print("Você é Psychic Eletric") 
+    elif dia == 2 or dia == 10 or dia == 21:
+        print("Você é Psychic Poison") 
+    elif dia == 3 or dia == 24:
+        print("Você é Psychic Normal") 
+    elif dia == 4 or dia == 12 or dia == 23:
+        print("Você é Psychic Dark") 
+    elif dia == 5 or dia == 27:
+        print("Você é Psychic Dragon") 
+    elif dia == 6 or dia == 16 or dia == 29:
+        print("Você é Psychic Ice") 
+    elif dia == 7 or dia == 18:
+        print("Você é Psychic Bug") 
+    elif dia == 8 or dia == 19 or dia == 30:
+        print("Você é Psychic Fairy") 
+    elif dia == 11 or dia == 22:
+        print("Você é Psychic Ghost") 
+    elif dia == 13 or dia == 26:
+        print("Você é Psychic Fire")
+    elif dia == 14 or dia == 25:
+        print("Você é Psychic Grass") 
+    elif dia == 15 or dia == 28:
+        print("Você é Psychic Water") 
+    elif dia == 20 or dia == 31:
+        print("Você é Psychic Rock") 
+    else:
+        print("Dia inválido")
+
+elif mes == 6 or mes == 10:
+    if dia == 1 or dia == 9 or dia == 17:
+        print("Você é Flying Eletric") 
+    elif dia == 2 or dia == 10 or dia == 21:
+        print("Você é Flying Poison") 
+    elif dia == 3 or dia == 24:
+        print("Você é Flying Normal") 
+    elif dia == 4 or dia == 12 or dia == 23:
+        print("Você é Flying Dark") 
+    elif dia == 5 or dia == 27:
+        print("Você é Flying Dragon") 
+    elif dia == 6 or dia == 16 or dia == 29:
+        print("Você é Flying Ice") 
+    elif dia == 7 or dia == 18:
+        print("Você é Flying Bug") 
+    elif dia == 8 or dia == 19 or dia == 30:
+        print("Você é Flying Fairy") 
+    elif dia == 11 or dia == 22:
+        print("Você é Flying Ghost") 
+    elif dia == 13 or dia == 26:
+        print("Você é Flying Fire")
+    elif dia == 14 or dia == 25:
+        print("Você é Flying Grass") 
+    elif dia == 15 or dia == 28:
+        print("Você é Flying Water") 
+    elif dia == 20 or dia == 31:
+        print("Você é Flying Rock") 
+    else:
+        print("Dia inválido")
+else:
+    print("Mês inválido!!") 
